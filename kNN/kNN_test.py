c51144f35b859cbe5a2fff61d995e3fd7ab8a7df
Wu-JM <wu_jiaming@outlook.com>
2020-06-14 13:57:52 +0200

    start over, and add some comments about the functions

--- kNN/kNN_test.py
+++ kNN/kNN_test.py
@@ -13,3 +13,21 @@ def createDataSet():  # create dataset and labels
     labels = ['A','A','B','B']
     return group, labels
 
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet  
+    # tile() to repeat inX into a same dimension set as the existing dataSet
+    sqDiffMat = diffMat**2
+    sqDistances = sqDiffMAt.sum(axis=1)
+    distances = sqDistances**0.5
+    sortedDistIndicies = distances.argsort()  
+    # .argsort() gets the indicies of the sorted elements
+    classCount = {}  # dictionary
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1  
+        # .get() return the value of the key (default 0)
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+    
+        
